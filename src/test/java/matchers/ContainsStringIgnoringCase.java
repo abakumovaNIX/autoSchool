854d0958d1e7143655d0d5a17cc7cedafa3439a1
Victoria Abakumova <abakumovaviktory@gmail.com>
2020-04-03 12:01:50 +0300

    10-Hamcrest Matchers - refactored according to "extends TypeSafeMatcher"

--- src/test/java/matchers/ContainsStringIgnoringCase.java
+++ src/test/java/matchers/ContainsStringIgnoringCase.java
@@ -1,25 +1,29 @@
 package matchers;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
-import org.hamcrest.core.SubstringMatcher;
 
-public class ContainsStringIgnoringCase extends SubstringMatcher {
+public class ContainsStringIgnoringCase extends TypeSafeMatcher<String>{
+
+   private String substring;
 
     public ContainsStringIgnoringCase(String substring) {
-        super(substring);
+        super();
+        this.substring = substring;
     }
 
-    public static TypeSafeMatcher containsStringIgnoringCase(String substring) {
+    @Override
+    protected boolean matchesSafely(String string) {
+        return string.toLowerCase().contains(substring.toLowerCase());
+    }
+
+    @Override
+    public void describeTo(Description description) {
+        description.appendText("string (ignore case)");
+    }
+
+    public static Matcher<String> containsStringIgnoringCase(String substring) {
         return new ContainsStringIgnoringCase(substring);
-    }
-
-    @Override
-    protected boolean evalSubstringOf(String s) {
-        return s.toLowerCase().contains(substring.toLowerCase());
-    }
-
-    @Override
-    protected String relationship() {
-        return "containing (ignoring case)";
     }
 }
